7074199f21d6ae2b3f4728adfcb88f885505b2ac
Seif Elnawawy <elnawawyseif@gmail.com>
2025-09-16 16:57:50 +0300

    added supabase

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,7 @@
+class AppSecrets {
+  AppSecrets._();
+
+  static const String supabaseUrl = 'https://fdvnmtyvcyrnxdyyvckq.supabase.co';
+  static const String supabaseApiKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkdm5tdHl2Y3lybnhkeXl2Y2txIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5NTg0ODAsImV4cCI6MjA3MzUzNDQ4MH0.emrOopI7ERNur-gyqZl9s1eFUCso6wnofVfW7N7Nw2M';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:e_commerce/core/secrets/app_secrets.dart';
 import 'package:e_commerce/ecommerce_app.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseApiKey,
+  );
   runApp(const ShopEase());
 }
