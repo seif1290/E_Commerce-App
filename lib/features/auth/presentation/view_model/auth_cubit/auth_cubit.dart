367decae05575048d58cadbc057abd45637cc4d1
Seif Elnawawy <elnawawyseif@gmail.com>
2025-09-21 08:33:42 +0300

    added login method to AuthCubit

--- lib/features/auth/presentation/view_model/auth_cubit/auth_cubit.dart
+++ lib/features/auth/presentation/view_model/auth_cubit/auth_cubit.dart
@@ -1,7 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:e_commerce/core/utils/constants/ui_constants/app_strings.dart';
+import 'package:e_commerce/features/auth/data/models/login_model.dart';
 import 'package:e_commerce/features/auth/data/models/register_model.dart';
-import 'package:e_commerce/features/auth/domain/entities/user_entity.dart';
+import 'package:e_commerce/core/shared/entities/user_entity.dart';
 import 'package:e_commerce/features/auth/domain/repos/auth_repo.dart';
 import 'package:flutter/widgets.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -28,6 +29,23 @@ class AuthCubit extends Cubit<AuthState> {
       );
     } catch (e) {
       debugPrint(e.toString());
+      emit(AuthState.failure(errMsg: AppStrings.somethingWrongMessage));
+    }
+  }
+
+  Future<void> login({required LoginModel loginModel}) async {
+    try {
+      emit(AuthState.loading());
+      final result = await _authRepo.login(loginModel: loginModel);
+      result.fold(
+        (failure) {
+          emit(AuthState.failure(errMsg: failure.message));
+        },
+        (userEntity) {
+          emit(AuthState.success(userEntity: userEntity));
+        },
+      );
+    } catch (e) {
       emit(AuthState.failure(errMsg: AppStrings.somethingWrongMessage));
     }
   }
