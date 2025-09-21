fc3236b83af9a90fca9d862045f869e2787224c6
Seif Elnawawy <elnawawyseif@gmail.com>
2025-09-21 08:56:09 +0300

    make Failure , LoacalException and ServerException  classes extends Equatable to implement  toString method

--- lib/core/error_handling/failure.dart
+++ lib/core/error_handling/failure.dart
@@ -1,5 +1,13 @@
-class Failure {
+import 'package:equatable/equatable.dart';
+
+class Failure extends Equatable {
   final String message;
   final String? code;
   const Failure({required this.message, this.code});
+
+  @override
+  List<Object?> get props => [message, code];
+
+  @override
+  bool? get stringify => true;
 }
--- lib/core/error_handling/local_exception.dart
+++ lib/core/error_handling/local_exception.dart
@@ -1,4 +1,12 @@
-class LocalException implements Exception {
+import 'package:equatable/equatable.dart';
+
+class LocalException extends Equatable implements Exception {
   final String message;
   const LocalException({required this.message});
+
+  @override
+  List<Object?> get props => [message];
+
+  @override
+  bool? get stringify => true;
 }
